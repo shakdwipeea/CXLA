39a4d4f2e22df6d818b6963dd25a88d9c5278083
rag594 <raghavrastogi594@gmail.com>
2015-10-28 22:18:48 +0530

    first test

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var fileLines = require('./routes/fileLines');
 
 var app = express();
 
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-
+app.use('/fileLines',fileLines);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
